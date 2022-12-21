c2701b21e5166051434c145667122b0aff5c4f68
Jeremy McDuffie <jmmcduffie@users.noreply.github.com>
2022-12-21 15:49:06 -0600

    [2022] Solve day 18 part 1

---
+++ 2022/18/part1.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const input = fs.readFileSync("./input.txt", "utf8").trim();
+
+const sum = (a, b) => a + b;
+
+const getAdjacentCubes = ([x, y, z]) => [
+  [x, y, z + 1],
+  [x, y, z - 1],
+  [x, y + 1, z],
+  [x, y - 1, z],
+  [x + 1, y, z],
+  [x - 1, y, z],
+];
+
+const cubes = input
+  .split("\n")
+  .map((cube) => cube.split(",").map((side) => parseInt(side, 10)));
+
+const solution = cubes.reduce(
+  ({ viewed, sides }, cube) => {
+    let adjacent = getAdjacentCubes(cube).map((c) => c.join(","));
+    sides += 6 - adjacent.filter((val) => viewed.includes(val)).length * 2;
+    viewed.push(cube.join(","));
+
+    return {
+      viewed,
+      sides,
+    };
+  },
+  { viewed: [], sides: 0 }
+);
+
+console.log(solution.sides);
